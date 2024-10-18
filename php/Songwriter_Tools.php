27cc3a0eb1ce71753a1fde2cc7bd0e0702ed72f0
Max Garceau <maxwellgarceau@gmail.com>
2024-10-18 00:31:19 -0600

    refactor: Sniff php with wordpress coding standards

--- php/Songwriter_Tools.php
+++ php/Songwriter_Tools.php
@@ -9,18 +9,18 @@ use Max_Garceau\Songwriter_Tools\Includes\Register_Songs_CPT;
  */
 class Songwriter_Tools {
 
-    /**
-     * Initialize the plugin.
-     */    
-    public function init() {
-        $this->register_hooks();
-    }
+	/**
+	 * Initialize the plugin.
+	 */
+	public function init() {
+		$this->register_hooks();
+	}
 
-    /**
-     * Register hooks for the plugin.
-     */
-    private function register_hooks(): void {
-        $register_songs_cpt = new Register_Songs_CPT();
-        add_action( 'init', [ $register_songs_cpt, 'create_song_post_type' ] );
-    }
+	/**
+	 * Register hooks for the plugin.
+	 */
+	private function register_hooks(): void {
+		$register_songs_cpt = new Register_Songs_CPT();
+		add_action( 'init', array( $register_songs_cpt, 'create_song_post_type' ) );
+	}
 }
