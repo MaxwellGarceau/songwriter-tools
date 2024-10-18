0a574c3d3674d0b0a288387932d11d67f5ffbabb
Max Garceau <maxwellgarceau@gmail.com>
2024-10-18 11:18:14 -0600

    feat: Add wp global object to TS types

---
+++ types/global.d.ts
@@ -0,0 +1,21 @@
+// Mark the file as a module to avoid being treated as a script
+// Required for global augmentation
+export {};
+
+declare global {
+    interface Window {
+        wp: {
+            interactivity: {
+                setContext: (key: string, value: any) => void;
+                getContext: (key: string) => any;
+            };
+            media: {
+                editor: {
+                    open: (target: any, options: any) => void;
+                    on: (event: string, callback: () => void) => void;
+                    state: () => any;
+                };
+            };
+        };
+    }
+}
