d17e7b36b72332a36baf654edf546019f90d389f
Max Garceau <maxwellgarceau@gmail.com>
2024-10-23 09:07:47 -0600

    feat: Add header font size support to FE

--- src/blocks/upload-song/render.php
+++ src/blocks/upload-song/render.php
@@ -53,7 +53,7 @@ $allowed_file_types_string = implode( '|', $allowed_file_types_labels );
 	data-wp-interactive="<?php echo $store_namespace; ?>"
 	<?php echo $context; ?>
 >
-	<<?php echo $attributes['headingTag']; ?>><?php echo esc_html__( $attributes['headingContent'], 'songwriter-tools' ); ?></<?php echo $attributes['headingTag']; ?>>
+	<<?php echo $attributes['headingTag']; ?> <?php if ( isset( $attributes['headingFontSize'] ) ) { echo 'style="font-size:' . $attributes['headingFontSize'] . '"'; } ?>><?php echo esc_html__( $attributes['headingContent'], 'songwriter-tools' ); ?></<?php echo $attributes['headingTag']; ?>>
 
 	<form id="song-upload-form" class="song-upload-form wp-block-form" data-wp-on--submit="actions.uploadSong">
 		<div class="song-upload-form__column">
